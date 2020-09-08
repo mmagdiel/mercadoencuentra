8a82d0f3ebacf92f419039d13d5f253232e15166
Magdiel Marquez <magdielikari@gmail.com>
2020-09-08 15:44:08 -0500

    Add test for alt images

--- client/src/presentational/navbar/Navbar.test.js
+++ client/src/presentational/navbar/Navbar.test.js
@@ -1,10 +1,26 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Navbar from "./Navbar";
-import { placeholderSearch } from "../../copies/navbar";
+import {
+  mercadolibreLogo,
+  searchIcon,
+  placeholderSearch,
+} from "../../copies/navbar";
 
-test("renders the correct placeholder", () => {
+test("render the correct placeholder", () => {
   const { getByPlaceholderText } = render(<Navbar />);
-  const linkElement = getByPlaceholderText(placeholderSearch);
-  expect(linkElement).toBeInTheDocument();
+  const placeholderText = getByPlaceholderText(placeholderSearch);
+  expect(placeholderText).toBeInTheDocument();
+});
+
+test("render the alt mercadolibreLogo image", () => {
+  const { queryByAltText } = render(<Navbar />);
+  const altText = queryByAltText(mercadolibreLogo);
+  expect(altText).toBeInTheDocument();
+});
+
+test("render the alt searchIcon image", () => {
+  const { queryByAltText } = render(<Navbar />);
+  const altText = queryByAltText(searchIcon);
+  expect(altText).toBeInTheDocument();
 });
